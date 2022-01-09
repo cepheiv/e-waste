1c35d76eafd67cab7a223a3f28cdb8a3901b7499
cepheiv <65541815+cepheiv@users.noreply.github.com>
2022-01-10 00:39:59 +0800

    Update map.js

--- JS/map.js
+++ JS/map.js
@@ -104,13 +104,13 @@ $("#searchButton").click(function() {
 
 // Set up map
 var center = L.bounds([1.51, 104.183], [1.16, 103.502]).getCenter();
-var map = L.map('mapdiv').setView([center.x, center.y], 11);
-var basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = L.map('mapdiv').setView([1.355, 103.835], 12);
+var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
   detectRetina: true,
   maxZoom: 19,
   minZoom: 11
 });
-//map.setMaxBounds([[1.56073, 104.153], [1.16, 103.502]]);
+map.setMaxBounds([[1.49, 104.153], [1.15, 103.6]]);
 basemap.addTo(map);
 
 function getLocation() {
